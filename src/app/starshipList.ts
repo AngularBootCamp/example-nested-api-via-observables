3440409b352c8ea600e25bcb3f1f16196ea2c190
Lance Finney <lance.finney@oasisdigital.com>
2019-12-06 21:55:08 -0600

    curriculum update

--- src/app/starshipList.ts
+++ src/app/starshipList.ts
@@ -5,7 +5,7 @@ import { IFilmMeta, StarshipApi } from './starshipApi';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'starshipList.html'
+  templateUrl: './starshipList.html'
 })
 export class StarshipListComponent {
   starships: Observable<IFilmMeta[]>;
